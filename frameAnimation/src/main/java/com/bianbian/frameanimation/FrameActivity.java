e17026e524672762bf92994cd1bf8cc6bacad5eb
bianjiangzhou <1456262162@qq.com>
2022-10-26 01:28:06 +0800

    添加线程池逐帧设置自定义帧动画

--- frameAnimation/src/main/java/com/bianbian/frameanimation/FrameActivity.java
+++ frameAnimation/src/main/java/com/bianbian/frameanimation/FrameActivity.java
@@ -2,6 +2,13 @@ package com.bianbian.frameanimation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bianbian.frameanimation.threadPool.CustomFrameAnim;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FrameActivity extends Activity {
 
@@ -11,6 +18,29 @@ public class FrameActivity extends Activity {
         setContentView(R.layout.frame_activity);
         findViewById(R.id.compensation_animation_back_text).setOnClickListener(view -> {
             finish();
+        });
+
+        ImageView imageView = findViewById(R.id.frame_img2);
+        TextView startText = findViewById(R.id.start_text);
+        TextView endText = findViewById(R.id.end_text);
+
+        List<Integer> resList = new ArrayList<>();
+        resList.add(R.drawable.frame_1);
+        resList.add(R.drawable.frame_2);
+        resList.add(R.drawable.frame_3);
+        resList.add(R.drawable.frame_4);
+        resList.add(R.drawable.frame_5);
+        resList.add(R.drawable.frame_6);
+        resList.add(R.drawable.frame_7);
+        resList.add(R.drawable.frame_8);
+
+        CustomFrameAnim anim = new CustomFrameAnim();
+        anim.setAnimBitmapRes(resList).bindImageView(imageView).setRepeat(true);
+        startText.setOnClickListener(v -> {
+            anim.start();
+        });
+        endText.setOnClickListener(v -> {
+            anim.stop();
         });
     }
 }
---
+++ frameAnimation/src/main/java/com/bianbian/frameanimation/threadPool/AnimThreadPool.java
@@ -0,0 +1,61 @@
+package com.bianbian.frameanimation.threadPool;
+
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class AnimThreadPool {
+    private static Map<String, ThreadPoolExecutor> map = new HashMap<>();
+
+    public static Future submit(String taskName, Runnable runnable) {
+        if (!map.containsKey(taskName)) {
+            map.put(taskName, getPoolExecutor(taskName));
+        }
+        return map.get(taskName).submit(runnable);
+    }
+
+    private static ThreadPoolExecutor getPoolExecutor(String taskName) {
+        return new ThreadPoolExecutor(
+                10,//核心线程数
+                10,//最大线程数
+                0,//线程数大于核心，多余空闲线程存活时间
+                TimeUnit.SECONDS,//时间单位
+                new LinkedBlockingDeque<>(10),//线程队列
+                /* 需将 runnable new Thread的构造方法中 */
+                runnable -> new Thread(runnable, taskName),//自定义名称线程池
+                new RejectHandler(taskName)//被拒策略
+        );
+    }
+
+    private static class RejectHandler implements RejectedExecutionHandler {
+        private String threadName;
+
+        public RejectHandler(String threadName) {
+            this.threadName = threadName;
+        }
+
+        /* 当线程池满，新提交的任务 runnable，会在这里返回；executor 为执行此任务的线程池 */
+        @Override
+        public void rejectedExecution(Runnable runnable, ThreadPoolExecutor executor) {
+            Log.e("ThreadPoolCustomAnim", "rejectedExecution");
+            if (map.containsKey(threadName)) {
+                map.get(threadName).shutdown();
+                map.remove(threadName);
+            }
+            executor.shutdown();
+            submit(threadName, runnable);
+        }
+    }
+}
